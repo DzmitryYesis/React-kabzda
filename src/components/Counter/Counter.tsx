c46e0767c933df0f5d75999b01b3d58ef373e118
DzmitryYesis <yesis1988@mail.ru>
2021-12-02 19:30:49 +0300

    Training with callback

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,23 +1,26 @@
 import React, {useState} from 'react';
+import s from './Counter.module.css'
 
 export const Counter = () => {
 
-    let [x,setX]=useState(0)
+    let [x, setX] = useState(0)
 
     const plus = () => {
-        setX(x+1)
+        setX(x + 1)
     }
     const minus = () => {
-        setX(x-1)
+        setX(x - 1)
     }
 
-    return(
-        <div>
+    return (
+        <div className={s.all}>
             <div>
-                <input value={x}/>
+                <input className={s.inp} value={x}/>
             </div>
-            <button onClick={plus}>+</button>
-            <button onClick={minus}>-</button>
+            <div className={s.but}>
+                <button onClick={plus}>+</button>
+                <button onClick={minus}>-</button>
+            </div>
         </div>
     )
 
