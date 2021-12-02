a5f11f313cfe7bd7331bb58340b8548797eee254
DzmitryYesis <yesis1988@mail.ru>
2021-12-02 19:00:31 +0300

    commit after problems with babel

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -5,14 +5,19 @@ export const Counter = () => {
     let [x,setX]=useState(0)
 
     const plus = () => {
-        setX(++x)
-        console.log(x)
-
+        setX(x+1)
+    }
+    const minus = () => {
+        setX(x-1)
     }
 
     return(
         <div>
+            <div>
+                <input value={x}/>
+            </div>
             <button onClick={plus}>+</button>
+            <button onClick={minus}>-</button>
         </div>
     )
 
